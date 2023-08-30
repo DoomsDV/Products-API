8dcbff81a69eee87da6875f9bac7a8257663303f
Daniel <danielvillasanti2132@gmail.com>
2023-08-30 19:58:07 -0400

    fix: se corrigieron errores en el admin.py

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -3,31 +3,27 @@ from .models import *
 
 @admin.register(Sub_category)
 class SubCategoryAdmin(admin.ModelAdmin):
-
-    class Meta:
-        model = Sub_category
-        fields = ('id', 'name')
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    search_fields = ['id', 'name']
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    sub_categories = SubCategoryAdmin() 
-
-    class Meta:
-        model = Category
-        fields = ('id', 'name', 'sub_categories')
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    filter_horizontal = ['sub_categories']
+    search_fields = ['id', 'name']
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    
-    class Meta:
-        model = Brand
-        fields = ('id', 'name')
+    list_display = ['id', 'name'] 
+    list_display_links = ['id', 'name']
+    search_fields = ['id', 'name']
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    brand = BrandAdmin()
-
-    class Meta:
-        model = Product
-        fields = ('id', 'name', 'code', 'description', 'brand', 'price'
-                  , 'category', 'sub_category', 'url_image')
+    list_display = ['id', 'name', 'code', 
+                    'brand']
+    list_display_links = ['id', 'name']
+    list_filter = ['category', 'brand', 'sub_category']
+    search_fields = ['id', 'name', 'code']

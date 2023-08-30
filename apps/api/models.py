e5edd89a4b367df997e57ec998e8ae8ca57172ef
Daniel <danielvillasanti2132@gmail.com>
2023-08-30 19:42:52 -0400

    Se creo el modelo para los productos

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import random
+import string
 
 class TimeLine(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
@@ -38,3 +40,26 @@ class Brand(TimeLine):
     def __str__(self):
         return self.name
 
+class Product(TimeLine):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=6, blank=True, null=True, unique=True)
+    description = models.TextField(blank=True, null=True)
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True)
+    price = models.PositiveBigIntegerField(default=0)
+    url_image = models.URLField()
+
+    class Meta:
+        verbose_name = 'producto'
+        verbose_name_plural = 'productos'
+
+    def generate_random_code(self):
+        characters = string.ascii_uppercase + string.digits
+        return ''.join(random.choice(characters) for _ in range(6))
+    
+    def save(self, *args, **kwargs):
+        if not self.code:
+            self.code = self.generate_random_code()
+        super(Product, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name

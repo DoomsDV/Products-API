340cad67a5321fca2149665f1f7a15bafc1c03e0
Daniel <danielvillasanti2132@gmail.com>
2023-08-30 20:46:54 -0400

    Se creo la url para las vistas de categorias

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -4,5 +4,10 @@ from .views import *
 app_name = 'api'
 urlpatterns = [
     path('products/', products_api_view, name='list-products-url'),
-    path('product/<int:id>/', product_api_view, name='get-product-url'),
+    path('product/<int:id>/', get_product_api_view, name='get-product-url'),
+]
+
+urlpatterns += [
+    path('categories/', categories_api_view, name='list-categories-url'),
+    path('category/<int:id>/', get_category_api_view, name='get-category-url'),
 ]

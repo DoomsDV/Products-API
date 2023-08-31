1aac73cba3b4322184da6b6bd7bde7e9a4fcb503
Daniel <danielvillasanti2132@gmail.com>
2023-08-30 20:22:28 -0400

    Se creo la vista para actualizar y eliminar un producto

--- apps/api/views.py
+++ apps/api/views.py
@@ -21,3 +21,24 @@ def products_api_view(req):
             return Response(product_serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_api_view(req, id):
+    product = Product.objects.filter(id=id).first()
+    if product:
+        if req.method == 'GET':
+            product_serializer = ProductSerializer(product)
+            return Response(product_serializer.data, status=status.HTTP_200_OK)
+        elif req.method == 'PUT':
+            product_serializer = ProductSerializer(product, data=req.data)
+            if product_serializer.is_valid():
+                product_serializer.save()
+                return Response(product_serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        elif req.method == 'DELETE':
+            product.delete()
+            return Response({'msg': 'The product was successfully removed'}, status=status.HTTP_200_OK)
+    else:
+        return Response({'msg': 'The product does not exist'}, status=status.HTTP_404_NOT_FOUND)

3803d1993c6190b9175ccf2405f0d26015c84e06
Daniel <danielvillasanti2132@gmail.com>
2023-09-02 17:25:40 -0400

    Se configuro la urls para produccion

--- src/urls.py
+++ src/urls.py
@@ -1,7 +1,13 @@
+import os
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.api.urls')),
 ]
+
+if os.environ.get('DJANGO_ENV') == 'production':
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)

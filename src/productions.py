14febdcabe5c35bc7be0ed6c107fde0c9cd4e87e
Daniel <danielvillasanti2132@gmail.com>
2023-09-02 17:21:38 -0400

    Se creo el archivo de configuracion para produccion

---
+++ src/productions.py
@@ -0,0 +1,23 @@
+from .settings import *
+import os
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = os.environ.get('DEBUG')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+MIDDLEWARE += [
+    'whitenoise.middleware.WhitenoiseMiddleware',
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
